fb4398e1f8ddc239e696865f4d0662b8d8349f44
mbower386 <mbower386@gmail.com>
2016-04-13 23:02:33 -0700

    Added MoveObject script.

--- Assets/Scripts/Controls/Command.cs
+++ Assets/Scripts/Controls/Command.cs
@@ -6,13 +6,25 @@ using GameInputNameSpace;
 public class Command : IControllable
 {
     #region Data Members
-	private string commandName;
-	private GameObject bodyReference;
+	protected string commandName;
+    protected CharacterInputs charMovement;   
 
     #endregion
 
     #region Setters & Getters
-    
+
+    public CharacterInputs Command_Type
+    {
+        get { return charMovement; }
+        set { charMovement = value; }
+    }
+
+    public string Command_Name
+    {
+        get { return commandName; }
+        set { commandName = value; }
+    }
+
     #endregion
 
     #region Built-in Unity Methods
@@ -20,10 +32,21 @@ public class Command : IControllable
     #endregion
 
     #region Public Methods
-    
+    public Command (string _commandName,  CharacterInputs _charMovement)
+    {
+        commandName = _commandName;
+        charMovement = _charMovement;
+    }
+
+    public void Execute (GameObject obj)
+    {
+
+    }
+
     #endregion
 
     #region Private Methods
-    		
+
     #endregion
 }
+
---
+++ Assets/Scripts/Controls/MoveObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using GameInputNameSpace;
+
+public class MoveObject : Command {
+
+	public MoveObject (string _commandName,  CharacterInputs _charMovement) : base (_commandName, _charMovement)
+    {
+        //base.charMovement = 
+    }
+}
