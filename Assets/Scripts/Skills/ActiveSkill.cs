fb529ce337a9419444cc7cca5e59b76e1c4ae9e4
Matthew Bower <mbower386>
2016-04-27 20:36:38 -0700

    Created base classes for the skill component.

---
+++ Assets/Scripts/Skills/ActiveSkill.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveSkill : BaseSkill 
+{
+	#region Data Members
+	protected int mpCost;
+	protected int staminaCost;
+
+	#endregion
+
+	#region Setters & Getters
+	public int Mp_Cost 
+	{
+		get { return mpCost; }
+		set { mpCost = value; }
+	}
+
+	public int Stamina_Cost 
+	{
+		get { return staminaCost; }
+		set { staminaCost = value; }
+	}
+
+	#endregion
+
+	#region Built-In Unity Methods
+
+	// Use this for initialization
+	protected void Start () 
+	{
+		base.Start ();
+		mpCost = 0;
+		staminaCost = 0;
+	}
+
+	// Update is called once per frame
+	protected void Update () 
+	{
+
+	}
+
+	#endregion
+
+	#region Public Methods
+	public virtual IExecutable SkillReference ()
+	{
+		IExecutable tmp = null;
+
+		return tmp;
+	}
+
+	#endregion
+
+	#region Private Methods
+
+	#endregion
+
+	#region Helper Classes/Structs
+
+	#endregion
+}
---
+++ Assets/Scripts/Skills/SkillInputTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillInputTest : MonoBehaviour {
+	public GameObject skill;
+	public GameObject cuck;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			skill.GetComponent<ActiveSkill> ().SkillReference().ActivateSkill();	
+		}
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			cuck.GetComponent<ActiveSkill> ().SkillReference().ActivateSkill();	
+		}
+	}
+}
